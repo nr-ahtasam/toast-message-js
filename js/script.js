405313d5d54288bbb608b032145f2b34d08addf0
naim_ahtasam <naim.ahtasam@gmail.com>
2023-09-28 05:09:53 +0600

    generate toast message click on copy button

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 //change the background color by generateing random hex color by clicking a button
 //also display the hex code to a disable input field
 // copy hexa code from the input field by clicking the copy button
+//add a toast message when copyed
+
+//global variavle
+
+let div = null;
 
 //step 1 create onload handler
 
@@ -21,6 +26,11 @@ function main() {
 
   copyBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(output.value);
+    if (div != null) {
+      div.remove();
+      div = null;
+    }
+    generateToastMessage(`${output.value} Copied Successfully`);
   });
 }
 
@@ -31,8 +41,30 @@ function generateHexColor() {
   const blue = Math.floor(Math.random() * 255);
   return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
+
+function generateToastMessage(msg) {
+  div = document.createElement("div");
+  div.innerText = msg;
+  div.className = "toast-message toast-animation-in";
+  document.body.appendChild(div);
+
+  div.addEventListener("click", () => {
+    div.classList.remove("toast-animation-in");
+    div.classList.add("toast-animation-out");
+    div.addEventListener("animationend", () => {
+      div.remove();
+      div = null;
+    });
+  });
+}
 // step 3 collect all necessary referances
 
 //step 4 handle the change button click event
 
-//step handle the copy button
+//step5 handle the copy button click event
+
+// step6 activate toast message
+
+// step 7 create dynamic toast message
+
+// step 8 clear dynamic toast message
